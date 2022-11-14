7285282af173eff5402072b1706144da1737eba5
Danil Zittser <d.zittser@ivideon.com>
2022-11-14 20:48:29 +0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Label-images
-How to label images to object detection tasks?
+# Разметка изображений
+Как размечать изображения для задачи детекции объектов?
+
+### Установка LabelImg
+
+Официальный репозиторий - [LabelImg](https://github.com/heartexlabs/labelImg).
+
+```bash
+# установка Python
+sudo apt install python3 python3-venv
+# создание виртуального окружения
+python3 -m venv venv
+# активация виртуального окружения
+source venv/bin/activate
+# обновление pip
+pip3 install --upgrade pip
+# установка зависимостей (labelImg и opencv-python)
+pip3 install -r requirements.txt
+# запуск LabelImg
+labelImg
+```
+
+### Настройка LabelImg
+
+[![](assets/readme-images/01-labelimg-set-up.png)](https://youtu.be/H6YZkKpGJK4)
+
+### Визуализация аннотаций в Python
+
+```bash
+python3 visualize.py --images-dir assets/example-dataset/images/ --annotations-dir assets/example-dataset/annotations/ --delay 5000
+```
+- `--images-dir` - путь до папки с изображениями
+- `--annotations-dir` - путь до папки с аннотациями
+- `--delay` - задержка между изображениями в миллисекундах
+
+![](assets/readme-images/02-visualization.png)
---
+++ visualize.py
@@ -0,0 +1,39 @@
+import argparse
+from pathlib import Path
+from typing import List
+
+import cv2
+import numpy as np
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--images-dir', type=Path, required=True, help='Path to the directory containing the images')
+    parser.add_argument('--annotations-dir', type=Path, required=True, help='Path to the directory containing the annotations')
+    parser.add_argument('--delay', default=5000, type=int, help='Visualization delay in milliseconds')
+    args = parser.parse_args()
+
+    assert args.images_dir.exists(), f'Images directory {args.images_dir} does not exist'
+    assert args.annotations_dir.exists(), f'Annotations directory {args.annotations_dir} does not exist'
+
+    images: List[Path] = sorted(args.images_dir.iterdir())
+    annotations: List[Path] = [args.annotations_dir / f'{image.stem}.txt' for image in images]
+
+    for image, annotation in zip(images, annotations):
+        image: np.ndarray = cv2.imread(str(image))
+        boxes: List[List[float]] = [list(map(float, x.split())) for x in annotation.read_text().splitlines()]
+
+        for (label, x_center, y_center, width, height) in boxes:
+            x1, y1 = int((x_center - width / 2) * image.shape[1]), int((y_center - height / 2) * image.shape[0])
+            x2, y2 = int((x_center + width / 2) * image.shape[1]), int((y_center + height / 2) * image.shape[0])
+
+            image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 3)
+            image = cv2.putText(image, f'{label:.0f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+
+        cv2.imshow('Press Q for exit', cv2.resize(image, (0, 0), fx=0.4, fy=0.4))
+        key = cv2.waitKey(-1) & 0xFF
+
+        if key == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
